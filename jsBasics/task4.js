d450ccb755131bf23f0c575e2025bf369fe7f04e
Dmitry <jokerdima801@gmail.com>
2024-04-28 19:52:58 +0300

    Fix isAdult variable

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,5 +1,12 @@
+// let age = 17;
+// let isAdult = true;
+
+// console.log(age);
+// console.log(age >= 18 ? isAdult : !isAdult);
+
 let age = 17;
-let isAdult = true;
+let isAdult = age >= 18;
+
 
 console.log(age);
-console.log(age >= 18 ? isAdult : !isAdult);
+console.log(isAdult);
