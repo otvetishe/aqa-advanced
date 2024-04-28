4ab8b3aebe29256ca5e6c62858ae7b018fb80ab4
Dmitry <jokerdima801@gmail.com>
2024-04-28 18:15:48 +0300

    Add rounded variables for 5.1 and 5.2

--- jsBasics/task5.js
+++ jsBasics/task5.js
@@ -2,16 +2,18 @@
 
 let radius = 5;
 let circumferense = Math.PI * radius * radius;
+let roundedCircumferense = circumferense.toFixed(2);
 
-console.log("Result for Task 5.1 is " + circumferense);
+console.log("Result for Task 5.1 is " + roundedCircumferense);
 
 // Task 5.2
 
 let length = 10;
 let width = 6;
 let areaOfRectangle = length * width;
+let roundedArea = areaOfRectangle.toFixed(2);
 
-console.log("Result for Task 5.2 is " + areaOfRectangle);
+console.log("Result for Task 5.2 is " + roundedArea);
 
 // Task 5.3
 
