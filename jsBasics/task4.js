94e31b745b037621d9f03ec899292b06058e5ac7
Dmitry <jokerdima801@gmail.com>
2024-04-28 18:05:49 +0300

    Fix gitignore file, done third homework

---
+++ jsBasics/task4.js
@@ -0,0 +1,5 @@
+let age = 17;
+let isAdult = true;
+
+console.log(age);
+console.log(age >= 18 ? isAdult : !isAdult);
---
+++ jsBasics/task5.js
@@ -0,0 +1,23 @@
+// Task 5.1
+
+let radius = 5;
+let circumferense = Math.PI * radius * radius;
+
+console.log("Result for Task 5.1 is " + circumferense);
+
+// Task 5.2
+
+let length = 10;
+let width = 6;
+let areaOfRectangle = length * width;
+
+console.log("Result for Task 5.2 is " + areaOfRectangle);
+
+// Task 5.3
+
+let radiusOfCylindre = 8;
+let height = 12;
+let volume = Math.PI * radiusOfCylindre *  radiusOfCylindre * height;
+let roundedVolume = volume.toFixed(2);
+
+console.log("Result for Task 5.3 is " + roundedVolume);
