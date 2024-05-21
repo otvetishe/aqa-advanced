fba4625c630fc6d5b601f3e2c754664d7f567fc5
Dmitry <jokerdima801@gmail.com>
2024-05-21 23:29:01 +0300

    Add hw 8.1

---
+++ jsBasics/HW8.1/task2.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5];
+const doubledWithIndex = numbers.map((number, index) => number * index);
+console.log(doubledWithIndex);
---
+++ jsBasics/HW8.1/task5.js
@@ -0,0 +1,6 @@
+const arr1 = [5, 10, 15];
+const arr2 = [13, 24, 35];
+
+const union = [...arr1, ...arr2];
+
+console.log(union);
